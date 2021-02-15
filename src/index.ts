732ad16513cf24dbebfa3160f9e81b0e61a7d40c
Paul Serby <serbypau@amazon.com>
2021-02-15 08:48:10 +0000

    Remove trail space from add style

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,21 @@ export type CssProperties = Properties;
 
 export const addStyle = (styles?: CssProperties): string =>
   styles && Object.keys(styles).length > 0
-    ? `(% style="${styleToCss(styles)}" %) `
+    ? `(% style="${styleToCss(styles)}" %)`
     : "";
 
 export const cell = (
   style: CssProperties,
   ...children: Array<string>
-): string => `|${addStyle(style)}${children.join("")}`;
+): string => `|${addStyle(style)} ${children.join("")}`;
 
 export const headerCell = (
   style: CssProperties,
   ...children: Array<string>
-): string => `|=${addStyle(style)}${children.join("")}`;
+): string => `|=${addStyle(style)} ${children.join("")}`;
 
 export const row = (style: CssProperties, ...children: Array<string>): string =>
-  `${addStyle(style)}${children.join("")}\n`;
+  `${addStyle(style)} ${children.join("")}\n`;
 
 export const multiline = (...children: Array<string>): string => `(((
 ${children.join("\n")}
@@ -35,12 +35,12 @@ export const htmlWithWiki = (
 ${children.join("")}
 {{/html}}`;
 
-export const h1 = (text: string): string => `=${text}=\n`;
-export const h2 = (text: string): string => `==${text}==\n`;
-export const h3 = (text: string): string => `===${text}===\n`;
-export const h4 = (text: string): string => `====${text}====\n`;
-export const h5 = (text: string): string => `=====${text}=====\n`;
-export const h6 = (text: string): string => `======${text}======\n`;
+export const h1 = (text: string): string => `= ${text} =\n\n`;
+export const h2 = (text: string): string => `== ${text} ==\n\n`;
+export const h3 = (text: string): string => `=== ${text} ===\n\n`;
+export const h4 = (text: string): string => `==== ${text} ====\n\n`;
+export const h5 = (text: string): string => `===== ${text} =====\n\n`;
+export const h6 = (text: string): string => `====== ${text} ======\n\n`;
 export const strong = (text: string): string => `**${text}**`;
 export const emphasis = (text: string): string => `//${text}//`;
 export const strike = (text: string): string => `--${text}--`;
@@ -48,7 +48,7 @@ export const mono = (text: string): string => `##${text}##`;
 export const superscript = (text: string): string => `^^${text}^^`;
 export const subscript = (text: string): string => `,,${text},,`;
 export const underline = (text: string): string => `__${text}__`;
-export const hr = (): string => `----\n`;
+export const hr = (): string => `\n----\n`;
 export const link = (text: string, href: string): string =>
   `[[${text}>>${href}]]`;
 
