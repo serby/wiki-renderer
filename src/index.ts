6ee65277ce06a0620c18cde426306ea8f543645a
Paul Serby <serbypau@amazon.com>
2021-02-15 08:34:44 +0000

    Fix h1 and add more syntax

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,11 @@ export const cell = (
   ...children: Array<string>
 ): string => `|${addStyle(style)}${children.join("")}`;
 
+export const headerCell = (
+  style: CssProperties,
+  ...children: Array<string>
+): string => `|=${addStyle(style)}${children.join("")}`;
+
 export const row = (style: CssProperties, ...children: Array<string>): string =>
   `${addStyle(style)}${children.join("")}\n`;
 
@@ -30,21 +35,26 @@ export const htmlWithWiki = (
 ${children.join("")}
 {{/html}}`;
 
-export const h1 = (text: string): string => `==${text}==\n`;
+export const h1 = (text: string): string => `=${text}=\n`;
 export const h2 = (text: string): string => `==${text}==\n`;
 export const h3 = (text: string): string => `===${text}===\n`;
 export const h4 = (text: string): string => `====${text}====\n`;
 export const h5 = (text: string): string => `=====${text}=====\n`;
 export const h6 = (text: string): string => `======${text}======\n`;
 export const strong = (text: string): string => `**${text}**`;
+export const emphasis = (text: string): string => `//${text}//`;
+export const strike = (text: string): string => `--${text}--`;
+export const mono = (text: string): string => `##${text}##`;
+export const superscript = (text: string): string => `^^${text}^^`;
+export const subscript = (text: string): string => `,,${text},,`;
 export const underline = (text: string): string => `__${text}__`;
 export const hr = (): string => `----\n`;
 export const link = (text: string, href: string): string =>
   `[[${text}>>${href}]]`;
 
-export const p = (text: string) => {
-  return `${text}\n`;
-};
+export const p = (text: string) => `${text}\n`;
+
+export const verbatim = (text: string) => `{{{${text}}}}`;
 
 export const table = row;
 
